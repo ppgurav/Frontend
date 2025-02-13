4a476430eb971761ab7430c8c619ad4d935e4714
ppgurav <priyankagurav190@gmail.com>
2025-02-13 10:45:39 +0530

    Update Dashboard.jsx

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { axiosInstance } from '../utils/axiosInstance';
 
 const Dashboard = () => {
+
+const [data, setData] = useState("")
+
+useEffect(() => {
+  async function fetchData(){
+    const res = axiosInstance.get('/user/call-logs')
+    if(res.status === 200){
+      setData(res.data.data)
+    }
+  } 
+  fetchData()
+}, [])
+
+
   return (
     <div>
-      <h2>Welcome</h2>
+      <h2>{data}</h2>
     </div>
   )
 }
