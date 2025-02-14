7d771a7d5bf6564fc61ccc803300c05386c3ad8b
ppgurav <priyankagurav190@gmail.com>
2025-02-14 20:38:52 +0530

    Update Navigationbar.jsx

--- src/Pages/Navigationbar.jsx
+++ src/Pages/Navigationbar.jsx
@@ -8,8 +8,8 @@ export default function Navigationbar() {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Call App</span>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-0 p-1">Profile</button>
-      <button type="button" className="text-white bg-black hover:bg-black-800 focus:ring-4 focus:outline-none focus:ring-black-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-black-600 dark:hover:bg-blue-700 dark:focus:ring-black-800 p-1">Logout</button>
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-1 p-1">Profile</button>
+      <button type="button" className="text-white bg-black hover:bg-black-800 focus:ring-4 focus:outline-none focus:ring-black-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-black-600 dark:hover:bg-blue-700 dark:focus:ring-black-800  m-1 p-1">Logout</button>
       <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
     </button>
   </div>
