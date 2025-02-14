bbd26a5893bc197dc02ab22855dea6adb385784c
ppgurav <priyankagurav190@gmail.com>
2025-02-14 20:38:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import Login from './Pages/Login';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Pages/Dashboard';
+import ProductList from './Pages/ProductList';
+import User from './Pages/User';
+import Navigationbar from './Pages/Navigationbar';
+
 function App() {
   return (
-    // <div className="App">
-    //   <Login />
-    // </div>
 
-    <BrowserRouter>
+<Router>
+  <Navigationbar />
     <Routes>
-      <Route path="/" element={<Login />}>
-      </Route>
-      <Route path="/signup" element={<Signup />}>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/product" element={<ProductList />} />
+        <Route path="/user" element={<User />} />
+        {/* <Route path="/nav" element={<Navigationbar />} /> */}
+      </Routes>
+</Router>
     
   );
 }
