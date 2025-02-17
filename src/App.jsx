a3cb11efa5811e30c671b5eec98f470cd46358df
ppgurav <priyankagurav190@gmail.com>
2025-02-17 11:17:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard';
 import ProductList from './Pages/ProductList';
 import User from './Pages/User';
-import Navigationbar from './Pages/Navigationbar';
+import UserCard from './Components/UserCard';
 
 function App() {
   return (
 
 <Router>
-  <Navigationbar />
     <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+         <Route path="/signup" element={<Signup />} /> 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/product" element={<ProductList />} />
         <Route path="/user" element={<User />} />
-        {/* <Route path="/nav" element={<Navigationbar />} /> */}
+        <Route path="/ucard" element={<UserCard />} />
+
+
       </Routes>
 </Router>
     
