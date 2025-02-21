4ba14fff8c8f21ac5bfa7f35c235b85919121f41
ppgurav <priyankagurav190@gmail.com>
2025-02-21 11:34:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Dashboard from './Pages/Dashboard';
 import ProductList from './Pages/ProductList';
 import User from './Pages/User';
 import UserCard from './Components/UserCard';
+import Profilepage from './Pages/Profilepage';
+import UpdatePassword from './Pages/UpdatePassword';
+import Compaign from './Pages/Compaign';
 
 function App() {
   return (
@@ -17,8 +20,11 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/product" element={<ProductList />} />
         <Route path="/user" element={<User />} />
+        <Route path="/compaign" element={<Compaign />} />
+        
         <Route path="/ucard" element={<UserCard />} />
-
+        <Route path="/profile" element={<Profilepage />} />
+        <Route path="/updatepassword" element={<UpdatePassword />} />
 
       </Routes>
 </Router>
