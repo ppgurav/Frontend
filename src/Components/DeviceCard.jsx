2e4e63a453b5aa93038ff7fe39182a767c984863
ppgurav <priyankagurav190@gmail.com>
2025-02-27 16:11:35 +0530

    Add files via upload

---
+++ src/Components/DeviceCard.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import { axiosInstance } from '../utils/axiosInstance';
+import { Eye, FileText, PowerOff, Trash2 } from 'lucide-react';
+
+
+const DeviceCard = ({user}) => {
+    const {
+        _id,deviceName,today_total_calls,overall_total_calls,webhookUrl} = user //{id:"",name:""}
+    const [data, setUser] = useState([]);
+                     useEffect(() => { 
+                        axiosInstance.get("/admin-and-reseller/devices")
+                         .then((res) => {
+                           if (res.status === 200) {
+                             console.log("Device", data);
+                             setUser(res.data.data || []);
+                           }
+                         })
+                         .catch((error) => {
+                           console.error("Error fetching data:", error);
+                           setUser([]);
+                         });
+                   
+                     }, []);
+    
+                     console.log("Device", data);
+                            
+  return (
+<>
+    <tr>
+              <td className="px-4 py-2 border">{_id}</td>
+              <td className="px-1 py-4 border-1 text-center flex flex-row" >{deviceName}<Eye className='text-blue-600 ml-15'/></td>
+              <td className="px-4 py-2 border text-center"><PowerOff /></td>
+              <td className="px-4 py-2 border text-center">{today_total_calls}</td>
+              <td className="px-4 py-2 border text-center">{overall_total_calls}</td>
+              <td className="px-4 py-2 border">{webhookUrl}</td>
+              <td className="p-2 flex  border-gray-200 border-1 ">
+              <FileText  className=''/>
+
+              <Trash2
+              className="text-red-500 cursor-pointer ml-4 "
+              size={23}  />
+
+             
+
+              </td>
+            </tr>
+            </>
+  )
+}
+
+export default DeviceCard;
