25969dab306b90d988e7b39b51c223ccd43aa534
ppgurav <priyankagurav190@gmail.com>
2025-03-04 10:17:58 +0530

    Update LogPage.jsx

--- src/Pages/LogPage.jsx
+++ src/Pages/LogPage.jsx
@@ -107,7 +107,8 @@ const CallLogs = () => {
             {callLogs.filter((log)=>{
               return search.toLowerCase() === '' ? log 
               : log.number.toLowerCase().includes(search) ||
-               log.deviceName.toLowerCase().includes(search);
+               log.deviceName.toLowerCase().includes(search) ||
+               log.type.toLowerCase().includes(search);
             })
             .map((log) => (
               <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200" key={log._id}>
