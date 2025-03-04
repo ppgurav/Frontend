1f1b12b17a34d14b7ce95c866b41ec1706aff1cc
ppgurav <priyankagurav190@gmail.com>
2025-03-04 16:25:15 +0530

    Update DeviceCard.jsx

--- src/Components/DeviceCard.jsx
+++ src/Components/DeviceCard.jsx
@@ -3,6 +3,7 @@ import { axiosInstance } from '../utils/axiosInstance';
 import { Eye, FileText, PowerOff, Trash2 } from 'lucide-react';
 
 
+
 const DeviceCard = ({user,device}) => {
     const {
         _id,deviceName,today_total_calls,overall_total_calls,webhookUrl} = user //{id:"",name:""}
@@ -43,12 +44,37 @@ const DeviceCard = ({user,device}) => {
                                   console.error(`Error occurred while deleting device with _id: ${_id}`, error);
                               });
                       };
-                            
+
+// it's webhookModal logic
+  const [isOpen, setIsOpen] = useState(false); 
+  const webhookURL = `http:/localhost:5173/webhook/call-log/${_id}`; 
+  const toggleModal = () => setIsOpen(!isOpen);
+
   return (
 <>
     <tr>
               <td className="px-4 py-2 border">{_id}</td>
-              <td className="px-1 py-4 border-1 text-center flex flex-row" >{deviceName}<Eye className='text-blue-600 ml-15'/></td>
+              <td className="px-1 py-4 border-1 text-center flex flex-row" >{deviceName}<Eye 
+              className="text-blue-500 cursor-pointer ml-1" onClick={toggleModal}
+              size={23}  />
+
+{isOpen && (
+       <div className="fixed inset-0 bg-transparent flex justify-center items-center">
+         <div className="bg-white p-20 rounded-lg shadow-lg w-200">
+           <div className="mb-4">
+             <p className="font-medium text-lg mr-130 my-2">Device Details</p>          
+               <p className='mr-19 '><strong>URL: </strong>{webhookURL} <button
+              onClick={toggleModal}
+              className="mt-4 ml-6 p-2 bg-red-500 text-white rounded-md"
+            >
+              Close
+            </button></p> 
+
+       </div>
+       </div>
+       </div>
+     )}
+              </td>
               <td className="px-4 py-2 border text-center"><PowerOff /></td>
               <td className="px-4 py-2 border text-center">{today_total_calls}</td>
               <td className="px-4 py-2 border text-center">{overall_total_calls}</td>
@@ -63,9 +89,6 @@ const DeviceCard = ({user,device}) => {
             >
                 <Trash2 size={24} color="#ff0000" />
             </button>
-
-             
-
               </td>
             </tr>
             </>
