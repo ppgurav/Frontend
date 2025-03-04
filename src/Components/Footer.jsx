3e081908fb6a3392e7ab392da97e62126d5dc076
ppgurav <priyankagurav190@gmail.com>
2025-03-04 16:23:07 +0530

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 text-black py-2 mt-6">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2025 Technfest IT Solution. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
